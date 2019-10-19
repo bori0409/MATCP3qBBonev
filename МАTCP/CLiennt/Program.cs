1a3611b3af88dc9be17cf4be7fa318f5cd35050c
bori0409 <44772249+bori0409@users.noreply.github.com>
2019-10-20 02:40:52 +0300

    ready
    unfixed

---
+++ МАTCP/CLiennt/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading.Tasks;
+namespace CLiennt
+{
+    
+        class Program
+        {
+            private static TcpClient _clientSocket = null;
+            private static Stream _nstream = null;
+            private static StreamWriter _sWriter = null;
+            private static StreamReader _sReader = null;
+            static void Main(string[] args)
+            {
+                try
+                {
+                    
+                    using (_clientSocket = new TcpClient("127.0.0.1", 7777))
+                    {
+                        using (_nstream = _clientSocket.GetStream())
+                        {
+                            
+                           _sWriter = new StreamWriter(_nstream) { AutoFlush = true };
+                        while (true)
+                        {
+                            Console.WriteLine("Write your message here Please note that if you want to save a book you should provide it in json format string in order to complate the saving command ");
+                            string clientMsg = Console.ReadLine();
+
+                            _sWriter.WriteLine(clientMsg);
+
+
+
+                            _sReader = new StreamReader(_nstream);
+                            string rdMsgFromServer = _sReader.ReadLine();
+                            if (rdMsgFromServer != null)
+                            {
+                                Console.WriteLine(".....................................................");
+                                Console.WriteLine("Client recieved Message from Server:" + rdMsgFromServer);
+                                Console.WriteLine(".....................................................");
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("Client recieved null message from Server ");
+                            }
+                        }
+                        }
+                    }
+                    Console.WriteLine("Press enter to stop the client!");
+                    Console.ReadKey();
+
+            }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    Console.ReadKey();
+                }
+            }
+
+        }
+    
+}
---
+++ МАTCP/ClassLibrary2/Book.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace ClassLibrary2
+{
+    public class Book
+    {
+        private string _title;
+        private string _author;
+        private int _pagenumber;
+        private string _isbn13;
+
+        public Book()
+        {
+
+        }
+        public Book(string title, string author, string isbn, int pages)
+        {
+            Title = title;
+            ISBN13 = isbn;
+            Author = author;
+            PageNumber = pages;
+        }
+         public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (value.Length > 2)
+                {
+                    _title = value;
+
+                }
+                else
+                {
+                    throw new Exception(" The Title of the book should be at least 2 chars");
+                }
+            }
+        }
+        public string ISBN13
+        {
+            get
+            {
+                return _isbn13;
+            }
+            set
+            {
+                if (value.Length > 12 && value.Length < 14)
+                {
+                    _isbn13 = value;
+
+                }
+                else
+                {
+                    throw new Exception(" ISBM must be 13 chars");
+                }
+            }
+        }
+
+        public int PageNumber
+        {
+            get
+            {
+                return _pagenumber;
+            }
+            set
+            {
+                if (value > 10 && value < 1001)
+                {
+                    _pagenumber = value;
+
+                }
+                else
+                {
+                    throw new Exception(" The pages of th ebook should be at least 10 and not more than 1001");
+                }
+            }
+            
+        }
+        public string Author
+        {
+            get
+            {
+                return _author;
+
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception(" The Author must be specified ");
+                }
+                else
+                {
+                    _author = value;
+                }
+            }
+        }
+        
+    }
+}
